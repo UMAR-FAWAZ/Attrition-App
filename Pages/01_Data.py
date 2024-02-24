a5d83337ad7315de08a43c500ec043e4e26c5c7d
Umar Fawaz <umarfawaz77@gmail.com>
2024-02-24 12:18:00 +0000

    Update data view page: get data from remote server

--- Pages/01_Data.py
+++ Pages/01_Data.py
@@ -1,10 +1,41 @@
 import streamlit as st 
+import pyodbc
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 st.set_page_config(
     page_title= 'View Data',
     page_icon=':)',
     layout= 'wide'
 )
+st.markdown(
+        "<h1 style='text-align: center; color: white; font-size: 36px;'>View Data</h1>",
+        unsafe_allow_html=True,
+)
 
-st.title('Proprietory Data from IBM')
+# Get data from remote Database 
 
+# Load environment variables from .env file 
+environment_variables = dotenv_values('.env')
+
+
+# Get the values for the credentials in the '.env' file
+database = environment_variables.get("DATABASE")
+server = environment_variables.get("SERVER")
+username = environment_variables.get("USER")
+password = environment_variables.get("PASSWORD")
+
+# Create a connection string
+connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};MARS_Connection=yes;MinProtocolVersion=TLSv1.2;"
+
+# Use the connect method of the pyodbc library and pass in the connection string.
+connection = pyodbc.connect(connection_string)
+
+# Execute the query and fetch the results
+query = "SELECT * FROM LP2_Telco_churn_first_3000"
+results = pd.read_sql(query, connection)
+
+#display the results in streamlit 
+st.dataframe(results)
